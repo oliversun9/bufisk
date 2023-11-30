d23052357db2fd5af4ee23563da7d85a170f1472
Oliver Sun <osun@buf.build>
2023-11-30 16:37:55 -0500

    use placeholder

--- scripts/draftrelease.bash
+++ scripts/draftrelease.bash
@@ -6,20 +6,23 @@ DIR="$(CDPATH= cd "$(dirname "${0}")/.." && pwd)"
 echo "DIR is $DIR"
 cd "${DIR}"
 
-# We already have set -u, but want to fail early if a required variable is not set.
-: ${RELEASE_MINISIGN_PRIVATE_KEY}
-: ${RELEASE_MINISIGN_PRIVATE_KEY_PASSWORD}
-# However, if you are already logged in for GitHub CLI locally, you can remove this line when running it locally.
-: ${GH_TOKEN}
+# TODO: uncomment
+# # We already have set -u, but want to fail early if a required variable is not set.
+# : ${RELEASE_MINISIGN_PRIVATE_KEY}
+# : ${RELEASE_MINISIGN_PRIVATE_KEY_PASSWORD}
+# # However, if you are already logged in for GitHub CLI locally, you can remove this line when running it locally.
+# : ${GH_TOKEN}
 
 if [[ "${VERSION}" == v* ]]; then
   echo "error: VERSION ${VERSION} must not start with 'v'" >&2
   exit 1
 fi
 
-make release
-unset RELEASE_MINISIGN_PRIVATE_KEY
-unset RELEASE_MINISIGN_PRIVATE_KEY_PASSWORD
+# TODO: uncomment
+# make release
+# unset RELEASE_MINISIGN_PRIVATE_KEY
+# unset RELEASE_MINISIGN_PRIVATE_KEY_PASSWORD
+echo "placeholder" > .build/release/bufisk/assets/placeholder.txt
 
 # The second v${VERSION} is the tag, see https://cli.github.com/manual/gh_release_create
 url=$(gh release create --draft --notes "${release_notes}" --title "v${VERSION}" "v${VERSION}" .build/release/bufisk/assets/*)

fbf82eef6056c6042ea006d84af0b237eef7b8eb
Oliver Sun <osun@buf.build>
2023-11-30 16:31:01 -0500

    init

---
+++ scripts/draftrelease.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DIR="$(CDPATH= cd "$(dirname "${0}")/../../.." && pwd)"
+cd "${DIR}"
+
+# We already have set -u, but want to fail early if a required variable is not set.
+: ${RELEASE_MINISIGN_PRIVATE_KEY}
+: ${RELEASE_MINISIGN_PRIVATE_KEY_PASSWORD}
+# However, if you are already logged in for GitHub CLI locally, you can remove this line when running it locally.
+: ${GH_TOKEN}
+
+if [[ "${VERSION}" == v* ]]; then
+  echo "error: VERSION ${VERSION} must not start with 'v'" >&2
+  exit 1
+fi
+
+make bufrelease
+unset RELEASE_MINISIGN_PRIVATE_KEY
+unset RELEASE_MINISIGN_PRIVATE_KEY_PASSWORD
+
+# The second v${VERSION} is the tag, see https://cli.github.com/manual/gh_release_create
+url=$(gh release create --draft --notes "${release_notes}" --title "v${VERSION}" "v${VERSION}" .build/release/bufisk/assets/*)
+
+echo "Release ${VERSION} has been drafted: ${url}"
